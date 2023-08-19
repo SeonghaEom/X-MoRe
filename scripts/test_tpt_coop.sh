1dda575cd87f8fda773472dbaeac985c13e6bd30
Seongha Eom <doubleb@kaist.ac.kr>
2023-08-19 19:29:01 +0900

    Update test_tpt_coop.sh

--- scripts/test_tpt_coop.sh
+++ scripts/test_tpt_coop.sh
@@ -6,9 +6,9 @@ arch=$3
 coop_weight=$4
 
 
-# python ensemble_with_entropy.py --test_sets=${dataset} --data='/data/seongha' --n_ctx=4 --ctx_init='a_photo_of_a' --gpu=${gpu} --arch=${arch} --retrieve_K=2 --seed=${seed} --load ${coop_weight}
-# python ensemble_with_entropy.py --test_sets=${dataset} --data='/data/seongha' --n_ctx=4 --ctx_init='a_photo_of_a' --gpu=${gpu} --arch=${arch} --retrieve_K=4 --seed=${seed} --load ${coop_weight}
-# python ensemble_with_entropy.py --test_sets=${dataset} --data='/data/seongha' --n_ctx=4 --ctx_init='a_photo_of_a' --gpu=${gpu} --arch=${arch} --retrieve_K=8 --seed=${seed} --load ${coop_weight}
+python ensemble_with_entropy.py --test_sets=${dataset} --data='/data/seongha' --n_ctx=4 --ctx_init='a_photo_of_a' --gpu=${gpu} --arch=${arch} --retrieve_K=2 --seed=${seed} --load ${coop_weight}
+python ensemble_with_entropy.py --test_sets=${dataset} --data='/data/seongha' --n_ctx=4 --ctx_init='a_photo_of_a' --gpu=${gpu} --arch=${arch} --retrieve_K=4 --seed=${seed} --load ${coop_weight}
+python ensemble_with_entropy.py --test_sets=${dataset} --data='/data/seongha' --n_ctx=4 --ctx_init='a_photo_of_a' --gpu=${gpu} --arch=${arch} --retrieve_K=8 --seed=${seed} --load ${coop_weight}
 python ensemble_with_entropy.py --test_sets=${dataset} --data='/data/seongha' --n_ctx=4 --ctx_init='a_photo_of_a' --gpu=${gpu} --arch=${arch} --retrieve_K=16 --seed=${seed} --load ${coop_weight}
 python ensemble_with_entropy.py --test_sets=${dataset} --data='/data/seongha' --n_ctx=4 --ctx_init='a_photo_of_a' --gpu=${gpu} --arch=${arch} --retrieve_K=32 --seed=${seed} --load ${coop_weight}
 python ensemble_with_entropy.py --test_sets=${dataset} --data='/data/seongha' --n_ctx=4 --ctx_init='a_photo_of_a' --gpu=${gpu} --arch=${arch} --retrieve_K=64 --seed=${seed} --load ${coop_weight}
